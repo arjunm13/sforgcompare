43e8d857d0b227cd0319cc8d2a615c18846f8e44
arjunm13 <arjunmahen13@gmail.com>
2018-03-28 21:23:36 -0400

    removed AWS

--- sforgcompare/settings.py
+++ sforgcompare/settings.py
@@ -109,10 +109,10 @@ SALESFORCE_API_VERSION = int(os.environ['SALESFORCE_API_VERSION'])
 
 
 # AWS Settings
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+#AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+#AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 # Mail Settings
 """
